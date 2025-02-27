bb1494c9c0620af8a2c5e979504ea239700e1482
Iori-Oshima <iorin619@gmail.com>
2025-02-27 15:30:44 +0900

    feat: [wip]apply tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-  <Layout>
-    <main>
-      <h1>Welcome to My Website</h1>
-      <p>This is the homepage.</p>
-    </main>
+    <Layout>
+      <section className="text-center">
+      <h1 className="text-4xl font-bold text-blue-700">Welcome to My Website</h1>
+      <p className="mt-2 text-gray-600">This is the homepage.</p>
+    </section>
   </Layout>
   );
 }

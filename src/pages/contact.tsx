076b7909750b8f9b13aa1bd035ed61703f778153
Iori-Oshima <iorin619@gmail.com>
2025-03-02 19:26:02 +0900

    feat: add ContactForm

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,14 @@
 import Layout from "@/components/Layout";
+import ContactForm from "@/components/ContactForm"
 
 export default function Contact() {
     return (
     <Layout>
-        <main>
-            <h1>Contact Us</h1>
-            <p>Get in touch with us.</p>
-        </main>
+        <section className="container mx-auto p-6">
+            <h1 className="text-3xl font-bold text-center mb-4">Contact Us</h1>
+                <p>Get in touch with us.</p>
+            <ContactForm />
+        </section>
     </Layout>
     );
 }

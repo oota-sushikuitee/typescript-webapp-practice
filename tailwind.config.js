1f02b4f9fb9007f1238c60b48ea00e8a45cc6cdb
Iori-Oshima <iorin619@gmail.com>
2025-03-13 22:41:06 +0900

    fix: delete src directory and add some directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
